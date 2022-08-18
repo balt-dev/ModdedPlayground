da72ff5ca5273aba2116eab9b0ac04374c6b4d67
Balt <heptor42@gmail.com>
2022-08-18 14:58:30 -0500

    yes

--- src/main/java/net/yummy/ExampleMod.java
+++ src/main/java/net/yummy/ExampleMod.java
@@ -10,7 +10,7 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("yummu");
 	public static ModConfig config;
 	@Override
 	public void onInitialize() {
